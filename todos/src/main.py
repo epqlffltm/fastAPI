116c876d027319f7189f99c1fc3f55f732f0bac3
epqlffltm <kjh55984249@gmail.com>
2026-04-13 01:25:34 +0900

    fastAPI 기본

---
+++ todos/src/main.py
@@ -0,0 +1,63 @@
+#main.py
+#2026-04-10
+
+#http://127.0.0.1:8000/docs
+
+from fastapi import FastAPI, Body, HTTPException
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class CreateTodoRequest(BaseModel):
+    id: int
+    contents: str
+    is_done: bool
+
+todo_data = {
+    1: {"id": 1, "contents": "test1", "is_done": True,},
+    2: {"id": 2, "contents": "test2", "is_done": False,},
+    3: {"id": 3, "contents": "test3", "is_done": False,},
+}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/number")
+async def get_number(number: Optional[int] = None):
+    return {"ping":"pong","number":number}
+
+@app.get("/todos")
+def get_todos_handler(order:str | None = None):
+    ret = list(todo_data.values())
+    if order == "DESC":
+        return ret[::-1]
+    return ret
+
+@app.get("/todos/{id}",status_code=200)
+def get_todos_handler(id: int):
+    todo = todo_data.get(id)
+    if todo:
+        return todo
+    raise HTTPException(status_code=404, detail="ToDo Not found")
+
+@app.post("/todos",status_code=201)
+def create_todos_handler(request: CreateTodoRequest):
+    todo_data[request.id] = request.model_dump()
+    return todo_data[request.id]
+
+@app.patch("/todos/{id}",status_code=200)
+def get_todos_handler(id: int, is_done: bool = Body(...,embed=True),):
+    todo = todo_data.get(id)
+    if todo:
+        todo["is_done"] = is_done
+        return  todo
+    raise HTTPException(status_code=404,detail="ToDo Not found")
+
+@app.delete("/todos/{id}", status_code=204)
+def delete_todos_handler(id: int):
+    todo = todo_data.pop(id, None)
+    if todo:
+        return
+    raise HTTPException(status_code=404, detail="ToTo Not found")
